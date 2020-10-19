07129e13e84c1dab202c5821687a586de0a30c68
zeefola <zeefola@gmail.com>
2020-10-19 09:19:43 +0100

    Added dropbox backup

--- app/Providers/DropboxServiceProvider.php
+++ app/Providers/DropboxServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+
+use Storage;
+use League\Flysystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
+use Spatie\Dropbox\Client as DropboxClient;
+use Spatie\FlysystemDropbox\DropboxAdapter;
 
 class DropboxServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,12 @@ class DropboxServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Storage::extend('dropbox', function ($app, $config) {
+            $client = new DropboxClient(
+                $config['authorization_token']
+            );
+ 
+            return new Filesystem(new DropboxAdapter($client));
+        });
     }
 }
