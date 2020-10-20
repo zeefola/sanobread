88559e7a9103fc7e6d848af876e365b5eb09162d
Promise Ezema <promiseezema11@gmail.com>
2020-10-20 20:59:05 +0100

    done

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/' , 'HomepageController@index');
-Route::get('/contact-us' , 'HomepageController@contactUs');
-Route::post('/contact-us' , 'HomepageController@contactUsConfirm');
+Route::get('/', 'HomepageController@index');
 Route::get('/about-us', 'HomepageController@about_us');
+Route::get('/contact-us' , 'HomepageController@contact_us');
