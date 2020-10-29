c1cda6fada59f77470e8bc56ec706ad1bc13251c
zeefola <zeefola@gmail.com>
2020-10-29 12:19:13 +0100

    breadcrumb schema markup

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -1,3 +1,18 @@
-<div class="pizzaro-breadcrumb">
-    <nav class="woocommerce-breadcrumb" ><a href="/">Home</a><span class="delimiter"><i class="po po-arrow-right-slider"></i></span>{{$title}}</nav>
+<div class="pizzaro-breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
+    <nav class="woocommerce-breadcrumb">
+        <span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" >
+        <a itemprop="item" href="/"> 
+            <span itemprop="name">Home</span> 
+        </a>
+        <meta itemprop="position" content="1" />
+        </span>
+
+        <span class="delimiter">
+            <i class="po po-arrow-right-slider"></i>
+        </span>
+        <span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+            <a itemprop="item" href="{{ request()->fullUrl() }}"> <span itemprop="name"> {{$title}}  </span> </a>
+            <meta itemprop="position" content="2" />
+        </span>
+    </nav>
  </div>
